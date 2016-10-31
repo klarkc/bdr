f5bc936ad757f6ae296c0caee08f7d86f2125f94
klarkc <walker@praiseweb.com.br>
2016-10-31 14:58:15 -0300

    refactory questao2

--- questao2.php
+++ questao2.php
@@ -1,10 +1,24 @@
-<?
+<?php
 
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-	header("Location: http://www.google.com");
-	exit();
-} elseif (isset($_COOKIE['Loggedin']) && $_COOKIE['Loggedin'] == true) {
-	header("Location: http://www.google.com");
-	exit();
+function executeIfLoggedIn($session, callable $func)
+{
+    if ($session &&
+        isset($_SESSION['loggedin']) &&
+        $_SESSION['loggedin'] == true
+    ) {
+            call_user_func($func);
+            return;
+    }
 
+    if (isset($_COOKIE['Loggedin']) && $_COOKIE['Loggedin'] == true) {
+        call_user_func($func);
+    };
 }
+
+function redirect()
+{
+    header("Location: http://www.google.com");
+}
+
+executeIfLoggedIn(true, 'redirect');
+exit();
