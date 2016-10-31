8dfb6afb4a0b4a0bb7fefbd4d91ae9ff9b0b3dd7
klarkc <walker@praiseweb.com.br>
2016-10-31 14:24:44 -0300

    fix phpcs errors

--- View/Helper/SessionExtHelper.php
+++ View/Helper/SessionExtHelper.php
@@ -19,7 +19,8 @@ class SessionExtHelper extends SessionHelper
   	 * Used to render the message set in Controller::Session::setFlash()
   	 *
   	 * @param string $key The [Message.]key you are rendering in the view.
-  	 * @param array  $attrs Additional attributes to use for the creation of this flash message. Supports the 'params', and 'element' keys that are used in the helper.
+  	 * @param array  $attrs Additional attributes to use for the creation of
+	 * this flash message. Supports the 'params', and 'element' keys that are used in the helper.
   	 * @return string string
   	 */
     public function flash($key = 'flash', $attrs = array())
