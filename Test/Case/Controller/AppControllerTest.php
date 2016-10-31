ac56700510b6802a928c745325d2603771c5228f
klarkc <walker@praiseweb.com.br>
2016-10-31 14:23:03 -0300

    remove not needed test

--- Test/Case/Controller/AppControllerTest.php
+++ Test/Case/Controller/AppControllerTest.php
@@ -4,37 +4,4 @@ App::uses('AppController', 'Controller');
 class AppControllerTest extends ControllerTestCase
 {
 
-       /**
-  	 * Fixtures
-  	 *
-  	 * @var array
-  	 */
-       public $fixtures = array();
-
-       public $mockUser = array(
-          'id' => 1,
-          'username' => 'admin',
-          'role' => 'admin'
-       );
-
-       protected $_controller;
-
-       /**
-  	 * Configura teste
-  	 *
-  	 * @return void
-  	 */
-    public function setUp()
-    {
-           $this->_controller = $this->generate('App', array(
-              'components' => array(
-                 'Session',
-                 'Auth' => array('user')
-              )
-           ));
-           $this->_controller->Auth
-              ->staticExpects($this->any())
-              ->method('user')
-              ->will($this->returnValue($this->mockUser));
-    }
 }
