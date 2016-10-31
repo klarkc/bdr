363c5f236fc85308b684002c1fed1ac9f3bfb1ee
klarkc <walker@praiseweb.com.br>
2016-10-31 13:54:22 -0300

    fix PagesController

--- Config/routes.php
+++ Config/routes.php
@@ -28,7 +28,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'public', 'index'));
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'index'));
 
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
