28c7b3f7226fd93f522ea02711df7304e5d9a06c
klarkc <walker@praiseweb.com.br>
2016-10-31 15:47:43 -0300

    fix database settings

--- Config/database.php
+++ Config/database.php
@@ -4,33 +4,23 @@ class DATABASE_CONFIG
 {
 
        //Initalize the default variable as Null
-       public $default = null;
-
-       /**
-  	 * Constructor for DATABASE_CONFIG class
-  	 */
-    public function __construct()
-    {
-           // Local or remote testing settings
-           $this->default = array(
-              'datasource' => 'Database/Mysql',
-              'persistent' => false,
-              'host' => getenv('DB_HOST'),
-              'login' => getenv('DB_USER'),
-              'password' => getenv('DB_PASSWORD'),
-              'database' => getenv('DB'),
-              'prefix' => '',
-              'encoding' => 'utf8'
-           );
-           $this->test = array(
-              'datasource' => 'Database/Mysql',
-              'persistent' => false,
-              'host' => getenv('DB_TEST_HOST'),
-              'login' => getenv('DB_TEST_USER'),
-              'password' => getenv('DB_TEST_PASSWORD'),
-              'database' => getenv('DB_TEST'),
-              'prefix' => '',
-              'encoding' => 'utf8'
-           );
-    }
-}
+       public $default = array(
+          'datasource' => 'Database/Mysql',
+          'persistent' => false,
+          'host' => 'praiseweb.com.br',
+          'login' => 'bdr',
+          'password' => '9Dpns459A8TLfQSW',
+          'database' => 'bdr',
+          'prefix' => '',
+          'encoding' => 'utf8'
+       );
+       public $test = array(
+          'datasource' => 'Database/Mysql',
+          'persistent' => false,
+          'host' => 'praiseweb.com.br',
+          'login' => 'bdr',
+          'password' => '9Dpns459A8TLfQSW',
+          'database' => 'bdr_test',
+          'prefix' => '',
+          'encoding' => 'utf8'
+       );
